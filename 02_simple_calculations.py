486ec1c2b966705d36f7bdd2a00ef63071161599
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2025-03-04 19:10:24 +0300

    [ADD] Implemented basic arithmetic and input handling scripts

---
+++ 02_simple_calculations.py
@@ -0,0 +1,11 @@
+# I will define two variables called a and b
+# I will ask a and b to the user
+# I will add, subtract, multiply and divide a and b
+# I will print the results
+a = int(input("Please, give me a number: "))
+b = int(input("Please, give me another number: "))
+
+print(f"The summation of {a} and {b} is {a + b}")
+print("The difference of", a, "and", b, "is", a - b)
+print("The product of", a, "and", b, "is", a * b)
+print("The division of", a, "and", b, "is", a / b)
---
+++ 03_conditionals.py
@@ -0,0 +1,6 @@
+a_number = 10
+
+if a_number > 10:
+    print("The number is greater than 10")
+else:
+    print("The number is not greater than 10")
