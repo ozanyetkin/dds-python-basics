4c266e05690f13cf51d803c5dce9d1d93f6dd5c2
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2025-03-04 22:21:26 +0300

    [UPDATE] HW for correct input validation for user number in multiplication script

--- 01_input_output.py
+++ 01_input_output.py
@@ -4,13 +4,16 @@ that will ask a number to the user
 and then it will multiply it by 10
 and then it will give the result
 """
+
 my_multiplier = 10
 
 user_number = input("Please, give me a number: ")
 
 # I need to check whether the user input is a number
 digits = "0123456789"
-if user_number == str(user_number):
+
+# Change the condition to make it work for valid numbers
+if user_number:
     print(user_number * my_multiplier)
 else:
     print("You did not give me a number. Please, try again.")
